dc49290da0018935bd75ce8f9960447a1dbf4cb8
Harish Ramachandran <harishsr.coder@gmail.com>
2015-05-24 10:57:17 -0400

    A6: Added seed data for Posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+100.times do |n|
+  Post.create!(
+    title: "This is title #{n}",
+    content: "This content is for title #{n}.  It is being written 20 times"*20,
+    phone: 1112223333,
+    email: "email#{n}@h_list.com",
+    # user_id: n
+  )
+end
